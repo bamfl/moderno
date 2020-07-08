dffc76a14f9b5ca56c31f7195b4f1104abd1915d
bamfl <petukhovrabota@mail.ru>
2020-07-08 15:20:44 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,25 @@
-$(function(){
-	var mixer = mixitup('.products__box');
+$(function(){ 
+	$(".item-products__rate-star").rateYo({
+	  rating: 5,
+	  starWidth: "12px",
+	  readOnly: true
+	});
+
+	$('.products-slider__inner').slick({
+		arrows: false,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		focusOnSelect: false,
+		dots: true,
+		responsive: [
+			{
+			  breakpoint: 767,
+			  settings: {
+				
+			  }
+			}
+		]
+	});
+
+	var mixer = mixitup('.products__box');	
 });
--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,8 @@ gulp.task('style', function(){
 	return gulp.src([
 		'node_modules/normalize.css/normalize.css',
 		'node_modules/slick-carousel/slick/slick.css',
-		'node_modules/magnific-popup/dist/magnific-popup.css'
+		'node_modules/magnific-popup/dist/magnific-popup.css',
+		'node_modules/rateyo/src/jquery.rateyo.css'
 	])
 		.pipe(concat('libs.min.css'))
 		.pipe(cssmin())
@@ -33,15 +34,15 @@ gulp.task('script', function(){
 	return gulp.src([
 		'node_modules/slick-carousel/slick/slick.js',
 		'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-		'node_modules/mixitup/dist/mixitup.js'
+		'node_modules/mixitup/dist/mixitup.js',
+		'node_modules/rateyo/src/jquery.rateyo.js'
 	])
 		.pipe(concat('libs.min.js'))
 		.pipe(uglify())
 		.pipe(gulp.dest('app/js'))
 });
 
-//Мой скрипт для создания main.min.js
-gulp.task('mainjs', function () {
+gulp.task('mainminjs', function () {
 	return gulp.src('app/js/main.js')
 		.pipe(rename({suffix:'.min'}))
 		.pipe(uglify())
@@ -73,4 +74,4 @@ gulp.task('watch', function(){
 	gulp.watch('app/js/*.js', gulp.parallel('js'))
 });
 
-gulp.task('default', gulp.parallel('style', 'mainjs', 'script','sass','watch','browser-sync'))
+gulp.task('default', gulp.parallel('style','mainminjs','script', 'sass','watch','browser-sync'))
